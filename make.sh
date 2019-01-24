9efcf8f582c207fd00b6e773182a2e735e47bdff
Hans Pabst <hans.pabst@intel.com>
2019-01-24 14:34:49 +0100

    Defining CC and undefining CXX apparently injects an alternative compiler.

--- make.sh
+++ make.sh
@@ -3,6 +3,9 @@
 HERE=$(cd $(dirname $0); pwd -P)
 
 export YA_CACHE_DIR=${HOME}/catboost-cache
+export AR=gcc-ar
+export CC=gcc
+unset CXX
 
 cd catboost/python-package/catboost
 ${HERE}/ya make -r -k -DHAVE_CUDA=no #-v
