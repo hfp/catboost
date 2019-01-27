50ffae04cc9debd8e80b8bbbb377f1a331425c13
Hans Pabst <hans.pabst@intel.com>
2019-01-27 17:30:54 +0100

    Attempt to avoid repeated memory allocation.

--- catboost/libs/algo/score_bin.h
+++ catboost/libs/algo/score_bin.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <util/generic/vector.h>
+#if !defined(SCORE_BIN_TLS)
+# include <util/system/tls.h>
+# define SCORE_BIN_TLS
+#endif
 
 #include <cmath>
 
@@ -14,9 +18,21 @@ struct TScoreBin {
 };
 
 // Helper function that calculates deterministic scores given bins with statistics for each split.
-inline TVector<double> GetScores(const TVector<TScoreBin>& scoreBin) {
+inline
+#if defined(SCORE_BIN_TLS)
+const TVector<double>&
+#else
+TVector<double>
+#endif
+GetScores(const TVector<TScoreBin>& scoreBin) {
     const int splitCount = scoreBin.ysize() - 1;
+#if defined(SCORE_BIN_TLS)
+    Y_STATIC_THREAD(TVector<double>) scores_local; // TVector is non-POD
+    TVector<double>& scores = TlsRef(scores_local);
+    scores.resize(splitCount);
+#else
     TVector<double> scores(splitCount);
+#endif
     for (int splitIdx = 0; splitIdx < splitCount; ++splitIdx) {
         scores[splitIdx] = scoreBin[splitIdx].GetScore();
     }
