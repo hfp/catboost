a1ded7c4cd96cc5c4ca31203354ecbd1fac569c9
Hans Pabst <hans.pabst@intel.com>
2019-02-05 09:24:19 +0100

    Slightly lowered StdNormalDistribution.

--- util/random/normal.h
+++ util/random/normal.h
@@ -14,10 +14,9 @@ static inline T StdNormalDistribution(TRng&& rng) noexcept {
 
     do {
         x = static_cast<T>(rng.GenRandReal1()) * T(2) - T(1);
-        r = x * x;
-    } while (T(0.5) < r || T(0) == x);
+    } while (T(M_SQRT1_2) < x || T(0) == x);
 
-    return std::sqrt(std::abs(T(M_LN2) + log(r)));
+    return std::sqrt(std::abs(T(M_LN2) + log(x * x)));
 }
 
 template <typename T, typename TRng>
