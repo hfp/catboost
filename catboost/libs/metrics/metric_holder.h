f4b9ed6469c891fc03495cfcc39089651bee32d0
Hans Pabst <hans.pabst@intel.com>
2019-02-10 17:51:55 +0100

    Turned TMetricHolder into a statically sized array (additional commits will add required/used member functions aka TVector compatibility).

--- catboost/libs/metrics/metric_holder.h
+++ catboost/libs/metrics/metric_holder.h
@@ -2,25 +2,34 @@
 
 #include <library/binsaver/bin_saver.h>
 
-#include <util/generic/vector.h>
+#include <util/generic/vector_ops.h>
 #include <util/system/yassert.h>
 
 // TODO(annaveronika): each metric should implement CreateMetricHolder(), CombineMetricHolders()
-struct TMetricHolder {
-    explicit TMetricHolder(int statsCount = 0) : Stats(statsCount) {}
-    TVector<double> Stats;
+struct TMetricHolder: public NVectorOps::TVectorOps<double, TMetricHolder> {
+    static constexpr int StatsCapacity = 4;
+    value_type Stats[StatsCapacity];
+    int StatsCount;
+
+    explicit TMetricHolder(int statsCount = 0) : StatsCount(statsCount) {
+        Y_VERIFY(StatsCount <= StatsCapacity);
+    }
 
     void Add(const TMetricHolder& other) {
         Y_VERIFY(Stats.empty() || other.Stats.empty() || Stats.size() == other.Stats.size());
-        if (other.Stats.empty()) {
-            return;
-        }
-        if (Stats.empty()) {
-            Stats = other.Stats;
-            return;
-        }
-        for (int i = 0; i < Stats.ysize(); ++i) {
-            Stats[i] += other.Stats[i];
+        Y_VERIFY(StatsCount <= StatsCapacity && other.StatsCount <= other.StatsCapacity);
+        if (!other.Stats.empty()) {
+            if (Stats.empty()) {
+                for (int i = 0; i < other.StatsCount; ++i) {
+                    Stats[i] = other.Stats[i];
+                }
+                StatsCount = other.StatsCount
+            }
+            else {
+                for (int i = 0; i < StatsCount; ++i) {
+                    Stats[i] += other.Stats[i];
+                }              
+            }
         }
     }
     SAVELOAD(Stats);
